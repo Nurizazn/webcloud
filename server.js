d3eba2006a28e35be817747d25104301eb2f3561
Nurizazn <abilnabila04@gmail.com>
2025-03-22 22:00:26 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,15 +7,14 @@ const app = express();
 
 // Konfigurasi koneksi ke database MySQL
 const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT || 3306,
-    ssl: { rejectUnauthorized: true }
+    host: 'webcloud-mysql.mysql.database.azure.com',
+    user: 'adminwebcloud@webcloud-mysql',
+    password: 'adminwebcc.01',
+    database: 'webcloud-app',
+    port: 3306,
+    ssl: require
 });
 
-// Coba koneksi ke database
 db.connect((err) => {
     if (err) {
         console.error('Koneksi database gagal:', err);
